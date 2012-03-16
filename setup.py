9c3798b2c9d720736c99b287d9c0a4dc9caaf2dd
Hector Velarde <hvelarde@gmail.com>
2012-03-16 21:42:15 +0000

    fixed package declaration

--- setup.py
+++ setup.py
@@ -1,21 +1,37 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import setup, find_packages
 import os
 
-version = open(os.path.join("Products", "ATGoogleVideo", "version.txt")).read().strip()
+version = '0.9.1dev'
+long_description = open("README.rst").read() + "\n" + \
+                   open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
+                   open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='Products.ATGoogleVideo',
       version=version,
-      description="Use YouTube and Google Video on a Plone Site",
-      long_description=open(os.path.join("Products", "ATGoogleVideo", "README.txt")).read().decode('UTF8').encode('ASCII', 'replace') + '\n' +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      description="Use YouTube and Google Video on a Plone Site.",
+      long_description=long_description,
       classifiers=[
+        "Development Status :: 6 - Mature",
+        "Environment :: Web Environment",
         "Framework :: Plone",
+        "Framework :: Plone :: 3.2",
+        "Framework :: Plone :: 3.3",
+        "Framework :: Plone :: 4.0",
+        "Framework :: Plone :: 4.1",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 2.6",
+        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
-      keywords='plone ATGoogleVideo youtube video googlevideo',
-      author='Simples Consultoria',
-      author_email='products@simplesconsultoria.com.br',
+      keywords='plone youtube video google',
+      author='Héctor Velarde',
+      author_email='hector.velarde@gmail.com',
       url='https://svn.plone.org/svn/collective/Products.ATGoogleVideo',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
@@ -23,11 +39,9 @@ setup(name='Products.ATGoogleVideo',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        ],
       entry_points="""
-      # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
       """,
