2216a5354a733a11b73dc0fa8b6f9361587baafe
Erico Andrei <eu@erico.com.br>
2012-04-05 13:13:34 +0000

    Back to development: 0.9.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.9.1'
+version = '0.9.2.dev0'
 long_description = open("README.rst").read() + "\n" + \
                    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
                    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
