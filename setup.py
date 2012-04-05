7618721bafd4df7d4cfa8919625b61930833b735
Erico Andrei <eu@erico.com.br>
2012-04-05 13:08:48 +0000

    Preparing release 0.9.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.9.1dev'
+version = '0.9.1'
 long_description = open("README.rst").read() + "\n" + \
                    open(os.path.join("docs", "INSTALL.txt")).read() + "\n" + \
                    open(os.path.join("docs", "CREDITS.txt")).read() + "\n" + \
